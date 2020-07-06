18b17a192d9536eafdd4d7203fd08537925fa53f
Allan Carvalho <allandcarv@gmail.com>
2020-07-06 20:45:37 +0100

    Components Isolation

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -29,41 +29,6 @@ export const Content = styled.div`
       margin-bottom: 24px;
     }
 
-    input {
-      background: #232129;
-      border-radius: 10px;
-      border: 2px solid #232129;
-      padding: 16px;
-      width: 100%;
-
-      color: #f4ede8;
-
-      &::placeholder {
-        color: #666360;
-      }
-
-      & + input {
-        margin-top: 8px;
-      }
-    }
-
-    button {
-      width: 100%;
-      height: 56px;
-      background: #ff9000;
-      border-radius: 10px;
-      border: 0;
-      padding: 0 16px;
-      margin-top: 16px;
-      font-weight: 500;
-      color: #312e38;
-      transition: background-color 0.2s;
-
-      &:hover {
-        background: ${shade(0.2, '#ff9000')};
-      }
-    }
-
     a {
       color: #f4ede8;
       display: block;
@@ -75,26 +40,26 @@ export const Content = styled.div`
         color: ${shade(0.2, '#f4ede8')};
       }
     }
-
   }
 
   > a {
-      color: #ff9000;
-      display: block;
-      margin-top: 24px;
-      text-decoration: none;
-      transition: color 0.2s;
+    color: #ff9000;
+    display: block;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: color 0.2s;
 
-      display: flex;
-      align-items: center;
+    display: flex;
+    align-items: center;
 
-      &:hover {
-        color: ${shade(0.2, '#ff9000')};
-      }
+    &:hover {
+      color: ${shade(0.2, '#ff9000')};
+    }
 
-      svg {
-        margin-right: 16px;
-      }
+    svg {
+      margin-right: 16px;
+    }
+  }
 `;
 
 export const Background = styled.div`
