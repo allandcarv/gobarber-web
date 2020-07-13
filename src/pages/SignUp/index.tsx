ee13a9c293b9fa9e5aead3fc94111d49fb4a202d
Allan Carvalho <allandcarv@gmail.com>
2020-07-13 20:37:59 +0100

    Inpu Usability

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -19,7 +19,7 @@ const SignUp: React.FC = () => {
       <Content>
         <img src={logoImg} alt="GoBarber" />
 
-        <Form onSubmit={handleSubmit}>
+        <Form initialData={{ name: 'Allan' }} onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
 
           <Input name="name" icon={FiUser} placeholder="E-Mail" />
